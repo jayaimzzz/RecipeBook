43a7bb8aae380fa53d2e624c09a48dbf9f93c68c
Michael McKenzie <mikemckenzie987@gmail.com>
2019-05-01 11:13:25 -0400

    fixed r chefs list of recipes

--- recipebook/views.py
+++ recipebook/views.py
@@ -11,14 +11,14 @@ def index(request):
 
 def recipe_content(request, id):
     html = "content.html"
-    recipe_list = {"recipes": Recipe.objects.all().filter(id=id)}
+    recipe_list = {"recipes": Recipe.objects.filter(id=id)}
     return render(request, html, recipe_list)
 
 
 def author(request, id):
     html = "author.html"
     items = {
-        "data": Author.objects.all().filter(id=id),
-        "recipes": Recipe.objects.all().filter(pk=id),
+        "data": Author.objects.filter(id=id),
+        "recipes": Recipe.objects.filter(author_id=id),
     }
     return render(request, html, items)
